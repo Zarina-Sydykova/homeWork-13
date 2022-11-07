31984288ec0c574454b4ec667cf963864bc2ff5e
zsydyk <zsydykova401@gmail.com>
2022-11-07 18:01:38 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyClass myClass = new MyClass("Zarina Sydykova", 27);
+        System.out.println(myClass.getFullName()+" "+myClass.getAge());
+        MyClass myClass1 = new MyClass(new String[]{"Java", "Soft skills", "English"},"Pasta: Fettuccine Alfredo");
+        System.out.println(Arrays.toString(myClass1.getCourses())+ " "+myClass1.getDish());
     }
 }
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class MyClass {
+public class MyClass {
+    private String fullName;
+    private  int age;
+    private String [] courses;
+    private String dish;
+
+    public MyClass(String fullName, int age){
+        this.fullName=fullName;
+        this.age=age;
+
+    }
+    public void setFullName(){
+        this.fullName=fullName;
+    }
+    public void setAge(){
+        this.age=age;
+    }
+    public String getFullName(){
+        return fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public MyClass(String [] courses, String dish){
+        this.courses=courses;
+        this.dish=dish;
+    }
+    public void setCourses(){
+        this.courses=courses;
+    }
+    public void setDish(){
+        this.dish=dish;
+    }
+    public String [] getCourses(){
+        return courses;
+    }
+    public String getDish(){
+        return dish;
+    }
 }
